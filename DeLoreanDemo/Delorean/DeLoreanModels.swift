e9e1ab889fa841aced99ea7b92bb7a889f723abe
JavierStori <javier.cervantes@storicard.com>
2024-04-22 14:10:34 -0600

    [Delete] DeLorean references

--- DeLoreanDemo/Delorean/DeLoreanModels.swift
+++ DeLoreanDemo/Delorean/DeLoreanModels.swift
@@ -10,6 +10,7 @@
 
 import UIKit
 
+
 struct DeLoreanModels: Codable {
     
     struct Request: Codable {}
@@ -18,21 +19,21 @@ struct DeLoreanModels: Codable {
     
     struct DeLoreanView: Codable {
         
-        private enum CodingKeys : String, CodingKey {
-            case dataLabel = "header"
-            case dataPrimaryButton = "primary_button"
-            case dataSecondaryButton = "secondary_button"
-            case dataIconButton = "icon_button"
-            case dataTextInput = "text_input"
-            case dataContainerView = "container_view"
-        }
-        
-        var dataLabel: DeLoreanLabelModel?
-        var dataPrimaryButton: DeLoreanPrimaryButtonModel?
-        var dataSecondaryButton: DeLoreanSecondaryButtonModel?
-        var dataIconButton: DeLoreanIconButtonModel?
-        var dataTextInput: DeLoreanTextInputModel?
-        var dataContainerView: DeLoreanContainerViewModel?
+//        private enum CodingKeys : String, CodingKey {
+//            case dataLabel = "header"
+//            case dataPrimaryButton = "primary_button"
+//            case dataSecondaryButton = "secondary_button"
+//            case dataIconButton = "icon_button"
+//            case dataTextInput = "text_input"
+//            case dataContainerView = "container_view"
+//        }
+//        
+//        var dataLabel: DeLoreanLabelModel?
+//        var dataPrimaryButton: DeLoreanPrimaryButtonModel?
+//        var dataSecondaryButton: DeLoreanSecondaryButtonModel?
+//        var dataIconButton: DeLoreanIconButtonModel?
+//        var dataTextInput: DeLoreanTextInputModel?
+//        var dataContainerView: DeLoreanContainerViewModel?
     }
 }
 
--- DeLoreanDemo/Delorean/DeLoreanViewController.swift
+++ DeLoreanDemo/Delorean/DeLoreanViewController.swift
@@ -14,29 +14,20 @@ class DeLoreanViewController: UIViewController {
     
     var interactor: DeLoreanInteractorProtocol!
     
-    var secondaryButton: DeLoreanSecondaryButton!
-    var primaryButton: DeLoreanPrimaryButton!
-    var iconButton: DeLoreanIconButton!
-    var textInput: DeLoreanTextInput!
+//    var secondaryButton: DeLoreanSecondaryButton!
+//    var primaryButton: DeLoreanPrimaryButton!
+//    var iconButton: DeLoreanIconButton!
+//    var textInput: DeLoreanTextInput!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         interactor.getDeLoreanData()
-//        for family in UIFont.familyNames {
-//            print("Font Family: \(family)")
-//
-//            // Enumerar todas las fuentes disponibles para esta familia
-//            for font in UIFont.fontNames(forFamilyName: family) {
-//                print("    \(font)")
-//            }
-//        }
-
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        setupView()
+//        setupView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -47,71 +38,71 @@ class DeLoreanViewController: UIViewController {
         super.viewDidDisappear(animated)
     }
     
-    private func setupView() {
-        guard let containerData = interactor.data?.dataContainerView else { return }
-        
-        let containerView = DeLoreanContainerView(data: containerData)
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(containerView)
-        containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        containerView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
-        containerView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -8).isActive = true
-        
-        guard let labelData = interactor.data?.dataLabel else { return }
-        let titleLabel = DeLoreanLabel(data: labelData)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(titleLabel)
-        titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20).isActive = true
-        titleLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 8).isActive = true
-        
-        guard let primaryButtonData = interactor.data?.dataPrimaryButton else { return }
-        primaryButton = DeLoreanPrimaryButton(data: primaryButtonData)
-        primaryButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        primaryButton.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(primaryButton)
-        primaryButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
-        primaryButton.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 8).isActive = true
-        primaryButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20).isActive = true
-        
-        guard let secondaryButtonData = interactor.data?.dataSecondaryButton else { return }
-        secondaryButton = DeLoreanSecondaryButton(data: secondaryButtonData)
-        secondaryButton.addTarget(self, action: #selector(buttonSPressed), for: .touchUpInside)
-        secondaryButton.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(secondaryButton)
-        secondaryButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
-        secondaryButton.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -8).isActive = true
-        
-        guard let iconButtonData = interactor.data?.dataIconButton else { return }
-        iconButton = DeLoreanIconButton(data: iconButtonData)
-        iconButton.addTarget(self, action: #selector(buttonIPressed), for: .touchUpInside)
-        iconButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(iconButton)
-        iconButton.topAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 20).isActive = true
-        iconButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
-        
-        guard let textInputData = interactor.data?.dataTextInput else { return }
-        textInput = DeLoreanTextInput(data: textInputData)
-        textInput.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(textInput)
-        textInput.topAnchor.constraint(equalTo: iconButton.bottomAnchor, constant: 20).isActive = true
-        textInput.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
-        textInput.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -8).isActive = true
-    }
+//    private func setupView() {
+//        guard let containerData = interactor.data?.dataContainerView else { return }
+//        
+//        let containerView = DeLoreanContainerView(data: containerData)
+//        containerView.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(containerView)
+//        containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+//        containerView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
+//        containerView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -8).isActive = true
+//        
+//        guard let labelData = interactor.data?.dataLabel else { return }
+//        let titleLabel = DeLoreanLabel(data: labelData)
+//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+//        containerView.addSubview(titleLabel)
+//        titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20).isActive = true
+//        titleLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 8).isActive = true
+//        
+//        guard let primaryButtonData = interactor.data?.dataPrimaryButton else { return }
+//        primaryButton = DeLoreanPrimaryButton(data: primaryButtonData)
+//        primaryButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+//        primaryButton.translatesAutoresizingMaskIntoConstraints = false
+//        containerView.addSubview(primaryButton)
+//        primaryButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+//        primaryButton.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 8).isActive = true
+//        primaryButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20).isActive = true
+//        
+//        guard let secondaryButtonData = interactor.data?.dataSecondaryButton else { return }
+//        secondaryButton = DeLoreanSecondaryButton(data: secondaryButtonData)
+//        secondaryButton.addTarget(self, action: #selector(buttonSPressed), for: .touchUpInside)
+//        secondaryButton.translatesAutoresizingMaskIntoConstraints = false
+//        containerView.addSubview(secondaryButton)
+//        secondaryButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+//        secondaryButton.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -8).isActive = true
+//        
+//        guard let iconButtonData = interactor.data?.dataIconButton else { return }
+//        iconButton = DeLoreanIconButton(data: iconButtonData)
+//        iconButton.addTarget(self, action: #selector(buttonIPressed), for: .touchUpInside)
+//        iconButton.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(iconButton)
+//        iconButton.topAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 20).isActive = true
+//        iconButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
+//        
+//        guard let textInputData = interactor.data?.dataTextInput else { return }
+//        textInput = DeLoreanTextInput(data: textInputData)
+//        textInput.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(textInput)
+//        textInput.topAnchor.constraint(equalTo: iconButton.bottomAnchor, constant: 20).isActive = true
+//        textInput.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
+//        textInput.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -8).isActive = true
+//    }
     
-    @objc func buttonPressed(_ sender: UIButton) {
-        // Cambiar el estado del botón
-        secondaryButton.isEnabled = !secondaryButton.isEnabled
-    }
-    
-    @objc func buttonSPressed(_ sender: UIButton) {
-        // Cambiar el estado del botón
-        iconButton.isEnabled = !iconButton.isEnabled
-    }
-    
-    @objc func buttonIPressed(_ sender: UIButton) {
-        // Cambiar el estado del botón
-        primaryButton.isEnabled = !primaryButton.isEnabled
-    }
+//    @objc func buttonPressed(_ sender: UIButton) {
+//        // Cambiar el estado del botón
+//        secondaryButton.isEnabled = !secondaryButton.isEnabled
+//    }
+//    
+//    @objc func buttonSPressed(_ sender: UIButton) {
+//        // Cambiar el estado del botón
+//        iconButton.isEnabled = !iconButton.isEnabled
+//    }
+//    
+//    @objc func buttonIPressed(_ sender: UIButton) {
+//        // Cambiar el estado del botón
+//        primaryButton.isEnabled = !primaryButton.isEnabled
+//    }
     
     
 }
